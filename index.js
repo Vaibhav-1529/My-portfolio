266fae8759892a2771dcac2c9b148de3099928df
Vaibhav-1529 <vaibhavsaini77786@gmail.com>
2024-05-22 13:04:06 +0530

    add javascript

---
+++ index.js
@@ -0,0 +1,12 @@
+function gotoskill(){
+    window.scrollTo(0,370)
+}
+function gotoproject(){
+    window.scrollTo(0,620)
+}
+function gotoedecation(){
+    window.scrollTo(0,900)
+}
+function gotocontact(){
+    window.scrollTo(0,900)
+}
